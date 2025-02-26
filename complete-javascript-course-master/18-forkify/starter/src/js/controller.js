d219ea81388f17463d3f6c964ca43e905ae44571
Ibrahim Suleiman Adamu <66827077+ibrahimsfura03@users.noreply.github.com>
2025-02-26 21:53:39 +0000

    forkify
    refactoring, MVC - architecture implementation

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+//Polyfilling
+import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +17,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+    // (1.) Loading the recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    // (2.) Rendering the recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipe)
+);
